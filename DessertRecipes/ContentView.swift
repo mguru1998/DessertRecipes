842140c594501e90c2bad15d9be5638e8340b209
Guruprasad <mguru1998@gmail.com>
2024-02-21 17:04:30 -0800

    Initial Commit

---
+++ DessertRecipes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DessertRecipes
+//
+//  Created by Guruprasad M on 2/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DessertRecipes/DessertRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertRecipesApp.swift
+//  DessertRecipes
+//
+//  Created by Guruprasad M on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
